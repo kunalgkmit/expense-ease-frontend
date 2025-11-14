f71670bb5b4ffbc2c1036428ae1215c03658a971
Kunal Prajapat <kunalprajapat@gkmit.co>
2025-11-14 14:50:30 +0530

    chore: remove default code from expo boilerplate

---
+++ app/_layout.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+
+export default function RootLayout() {
+  return (
+    <>
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack/>
+      </Stack>
+
+      <StatusBar style="auto" />
+    </>
+  );
+}
