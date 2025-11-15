dc49db3c8359a5528e1418ce650359cc5534b59e
Kunal Prajapat <kunalprajapat@gkmit.co>
2025-11-15 18:03:21 +0530

    chore: initial setup of flutter app

---
+++ lib/sample.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Sample extends StatelessWidget {
+  const Sample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [Text("Welcome to sample flutter")],
+        ),
+      ),
+    );
+  }
+}
